e1c3de1813d130f06c9ec4ae5fcae0ebbc8c8508
Yifan Wu <yifanfwu@gmail.com>
2015-01-31 10:31:17 -0500

    added app bar

--- app/src/main/java/com/clv69/alarmnfc/MainActivity.java
+++ app/src/main/java/com/clv69/alarmnfc/MainActivity.java
@@ -4,14 +4,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
 
---
+++ app/src/main/java/com/clv69/alarmnfc/TimePickerDialogFragment.java
@@ -0,0 +1,42 @@
+package com.clv69.alarmnfc;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link TimePickerDialogFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link TimePickerDialogFragment# newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TimePickerDialogFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+}
